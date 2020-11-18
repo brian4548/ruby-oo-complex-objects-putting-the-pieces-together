37aa4867f71ac07dfb94d7654578069ee54d60ee
brian4548 <brianr4548@gmail.com>
2020-11-17 23:47:25 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+require 'pry'
 
+class Book
+
+attr_reader :title, :author, :page_count, :genre
+attr_writer :author
+
+def initialize(title)
+  @title = title
+end
+
+def page_count=(num)
+    @page_count = num
+end
+
+def genre=(genre)
+    @genre = genre
+end
+
+def turn_page
+    puts "Flipping the page...wow, you read fast!"
+end
+
+end
+info = Book.new("And Then There Were None")
 
